b09a6a962ae7e4aef7ecaff54fe4cc019885958d
Shimizuke <kei.s61.0908@gmail.com>
2020-08-25 13:27:30 +0900

    accountアプリ追加

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    'register.apps.RegisterConfig',#login追加
+    'django.contrib.admin', #migrate時にいったんコメントアウト(urls.py admin/~も同様)
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -104,18 +105,25 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'ja'
+TIME_ZONE = 'Asia/Tokyo'
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# カスタムユーザーを使う
+AUTH_USER_MODEL = 'register.User'
+# ログインページと、直接ログインページへ行った後のリダイレクトページ
+LOGIN_URL = 'register:login'
+LOGIN_REDIRECT_URL = 'register:top'
+# メールをコンソールに表示する
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# 画像保存
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- todoproject/urls.py
+++ todoproject/urls.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+#画像保存ここから
+from . import settings
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+#画像保存ここまで
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),#migrate時に一旦コメントアウト()
     path('', include('todo.urls')),
+    path('', include('register.urls')),
 ]
+
+#画像保存ここから
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#画像保存ここまで
